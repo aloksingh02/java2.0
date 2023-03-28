eb4b261eed1f0551ad3944882aae9fdd83a45737
Alok KumarS Singh <singhalokkumar427@gmail.com>
2023-03-29 02:22:25 +0530

    date type

---
+++ src/DATATYPE_ALL.java
@@ -0,0 +1,28 @@
+public class DATATYPE_ALL {
+    public static void main(String[] args) {
+        //it simply means that which type of data is.
+        //it specify the different size and value that can be store
+        // in the variable.
+        // there are 8 type of variable
+        //int,short,byte,char,double,long,boolean,float.
+
+        char a = 'g';
+        int i = 56;
+         byte b = 120;
+         short s =56;
+         float f= 56.26f;
+         double d=56.65;
+         long l = 65684;
+         boolean one = true;
+
+        System.out.println(a);
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(b);
+
+    }
+}
